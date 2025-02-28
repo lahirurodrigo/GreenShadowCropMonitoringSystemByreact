94b3c6a775faae9075336e7f36793c2dd8fb6a01
lahirurodrigo <kmlahirurodrigo@gmail.com>
2025-03-01 03:41:16 +0530

    add styles

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -8,7 +8,7 @@ const card = ({children}: CardComponentProps) => {
     return (
         <>
             <div
-                className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+                className="max-w-6xl p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                 {children}
             </div>
         </>
